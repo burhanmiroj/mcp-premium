660a2bb9d64114cde49c6e9c8827e2fbb6e9bfd2
burhanmiroj <mirojburhanudin2@gmail.com>
2025-08-21 12:18:31 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    purge: true,
+    theme: {
+        extend: {
+            fontFamily: {
+                rethink: ['Rethink Sans', 'sans-serif'],
+                inter: ['Inter', 'sans-serif'],
+                bebas: ['Bebas Neue', 'sans-serif'],
+            }
+        }
+    },
+    plugins: [],
+};
