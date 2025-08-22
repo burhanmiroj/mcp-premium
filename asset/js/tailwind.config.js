90f1cf5301b379f4bf01f275bc457b403252edea
burhanmiroj <mirojburhanudin2@gmail.com>
2025-08-22 18:41:10 +0700

    Fix base setup tailwindcss

---
+++ asset/js/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                rethink: ['"Rethink Sans"', 'sans-serif'],
+                inter: ['Inter', 'sans-serif'],
+                bebas: ['"Bebas Neue"', 'sans-serif'],
+            },
+            colors: {
+                gray: {
+                    primary: '#0B0B0B',
+                    secondary: '#181A1C',
+                }
+            }
+        }
+    }
+}
